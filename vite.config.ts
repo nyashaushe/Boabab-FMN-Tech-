d8f2084b5c4ac93e16f894b121d2bd574dd937b1
Nyasha Shepard Ushewokunze <mrshepard18@gmail.com>
2025-02-28 09:26:10 +0200

    new fils

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,25 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/baobab-stack/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name].[hash][extname]`;
+          }
+          return `assets/[name].[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js',
+      },
+    },
+  },
   server: {
     port: 3000,
     host: true,
